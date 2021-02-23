b753a6d99f8c39d6d1e0a6b0babe00c0746a01c1
ParkSungHee <di6223@naver.com>
2021-02-23 22:03:46 +0900

    Revert "test"

--- app/src/main/java/com/threeans/cafe/SideNavigation.kt
+++ app/src/main/java/com/threeans/cafe/SideNavigation.kt
@@ -12,6 +12,9 @@ class SideNavigation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_side_navigation)
 
+
+
+
     }
 
     override fun onBackPressed() {
